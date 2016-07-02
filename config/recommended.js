9e6f5aae8b9df1fd193312d45e62af255fb81672
Alex LaFroscia <alexlafroscia@nestlabs.com>
2016-07-02 16:51:14 -0400

    Use Polyjuice to port over JSCS recommended config

--- config/recommended.js
+++ config/recommended.js
@@ -6,12 +6,73 @@ module.exports = {
   ],
   rules: {
     // Built-in Rules
-    'prefer-const': ['error'],
+    'no-empty': 'error',
+    'brace-style': 'error',
+    'no-multiple-empty-lines': 'error',
+    'one-var': ['error', {
+      'uninitialized': 'always',
+      'initialized': 'never'
+    }],
+    'operator-linebreak': ['error', 'before'],
+    'key-spacing': ['error', {
+      'beforeColon': false,
+      'afterColon': true
+    }],
+    'space-unary-ops': ['error', {
+      'words': false,
+      'nonwords': false
+    }],
+    'semi-spacing': ['error', {
+      'before': false,
+      'after': true
+    }],
+    'space-before-function-paren': ['error', 'never'],
+    'space-in-parens': ['error', 'never'],
+    'no-spaced-func': 'error',
+    'comma-dangle': ['error', 'never'],
+    'no-trailing-spaces': 'error',
+    'no-var': 'error',
+    'camelcase': ['error', {
+      'properties': 'never'
+    }],
+    'new-cap': 'error',
+    'comma-style': ['error', 'last'],
+    'curly': ['error', 'all'],
+    'dot-notation': 'error',
+    'object-shorthand': 'error',
+    'newline-after-var': ['error', 'always'],
+    'arrow-parens': 'error',
+    'semi': ['error', 'always'],
+    'space-infix-ops': 'error',
+    'keyword-spacing': ['error', {
+      'overrides': {
+        'else': {
+          'before': true
+        },
+        'while': {
+          'before': true
+        },
+        'catch': {
+          'before': true
+        }
+      }
+    }],
+    'spaced-comment': ['error', 'always'],
+    'space-before-blocks': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'always'],
+    'prefer-spread': 'error',
+    'prefer-template': 'error',
+    'indent': ['error', 2, {
+      'SwitchCase': 1
+    }],
+    'quotes': ['error', 'single', {
+      'avoidEscape': true
+    }],
 
     // Ember Suave rules
-    'ember-suave/no-const-outside-module-scope': ['error'],
-    'ember-suave/no-direct-property-access': ['error'],
-    'ember-suave/require-access-in-comments': ['error'],
-    'ember-suave/require-const-for-ember-properties': ['error']
+    'ember-suave/no-const-outside-module-scope': 'error',
+    'ember-suave/no-direct-property-access': 'error',
+    'ember-suave/require-access-in-comments': 'error',
+    'ember-suave/require-const-for-ember-properties': 'error'
   }
 };
