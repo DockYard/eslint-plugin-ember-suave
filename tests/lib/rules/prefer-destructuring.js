83e144f1745070e3f458fb754d6a3318ec4fb275
Alex LaFroscia <alex@lafroscia.com>
2016-07-31 15:19:29 -0700

    Prevent destructuring when property and variable are different
    Closes #13

--- tests/lib/rules/prefer-destructuring.js
+++ tests/lib/rules/prefer-destructuring.js
@@ -34,6 +34,10 @@ ruleTester.run("prefer-destructuring", rule, {
       code: "var foo;"
     },
     {
+      code: "var foo = object.bar;",
+      parserOptions: { ecmaVersion: 6 }
+    },
+    {
       code: "var foo = array[0];",
       options: [{ array: false }],
       parserOptions: { ecmaVersion: 6 }
